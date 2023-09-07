3d44f13e0b0888732f290416b8082f68447cc4ce
sangita paul <135009752+sangitagit@users.noreply.github.com>
2023-09-07 15:13:27 +0530

    Update contact.service.ts

--- src/app/contact.service.ts
+++ src/app/contact.service.ts
@@ -8,7 +8,7 @@ import { Contact } from './contact';
 })
 export class ContactService {
 
-  private baseUrl="http://18.191.194.126:8080/"
+  private baseUrl="http://3.143.9.35:8080/"
 
   constructor(private httpClient:HttpClient) { }
 
